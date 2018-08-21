19f8c4248ee52a6e57f20068c1104a1291bc54d5
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function takeANumber (current, new_person){
   } else {
     current.push(new_person)
   }
-  return console.log(`Welcome, ${new_person}. You are number ${current.length + 1} in line.`)
+  return `Welcome, ${new_person}. You are number ${current.length + 1} in line.`
 }
 function nowServing (current){
   var message = []
   if (current.length > 0){
     var customer = current[0]
     current = current.splice(current.length - 1)
-    return customer
+    return `Currently serving ${customer}.`
   }
   return 'There is nobody waiting to be served!'
 }
