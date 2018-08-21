52f25f0797ff452064c09525fbf681d95c9dad36
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (current, new_person){
   var temp =[]
   temp.concat(current)
-  return console.log(`Welcome, ${new}. You are number ${temp.length + 1} in line.`)
+  return console.log(`Welcome, ${new_person}. You are number ${temp.length + 1} in line.`)
 }
