82ce187bd9d2c5aefe05ed9ecd2ba975e94ee5f1
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function nowServing (current){
   }
   return 'There is nobody waiting to be served!'
 }
+function currentLine (current){
+  var sentence = 'The line is currently:'
+  for ( var i = 0 ; i < current.length ; i++){
+    if (i < current.length){
+    sentence = sentence + ` ${i + 1}. ${current[i]},`
+    } else {
+      sentence = sentence + 
+    }
+  
+  }
+}
