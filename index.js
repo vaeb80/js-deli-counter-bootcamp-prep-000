3e0cd95c5fd08b06621efefc2155a4af54577c7f
V Echeveste <vaeb80@gmail.com>
2018-08-21 03:01:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function takeANumber (current, new_person){
   return `Welcome, ${new_person}. You are number ${current.length } in line.`;
 }
 function nowServing (current){
-  var message = []
+  var message = [];
   if (current.length > 0){
-    var customer = current[0]
-    current.shift()
-    return `Currently serving ${customer}.`
+    var customer = current[0];
+    current.shift();
+    return `Currently serving ${customer}.`;
   }
-  return 'There is nobody waiting to be served!'
+  return 'There is nobody waiting to be served!';
 }
 function currentLine (current){
   var sentence = 'The line is currently'
