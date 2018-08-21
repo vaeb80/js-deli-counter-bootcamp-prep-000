db38be468d033a651aa3f28ebc34d57475352ca8
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function takeANumber (current, new_person){
+  if (current.length == 0){
+    current = [new_person]
+  }
   current.push(new_person)
   return console.log(`Welcome, ${new_person}. You are number ${current.length + 1} in line.`)
 }
