483dc2aeb3f6509a4fa6b819f2e356079cf9163a
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber (current, new_person){
   if (current.length == 0){
-    current = [new_person]
+    current.unshift(new_person)
   }
   current.push(new_person)
   return console.log(`Welcome, ${new_person}. You are number ${current.length + 1} in line.`)
