a315456e3a2019e7447b4b166c55f5091cc85350
V Echeveste <vaeb80@gmail.com>
2018-08-21 02:26:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber (current, new_person){
-  var temp =[]
-  temp.concat(current)
+  current.push(new_person)
   return console.log(`Welcome, ${new_person}. You are number ${temp.length + 1} in line.`)
 }
